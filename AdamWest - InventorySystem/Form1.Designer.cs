d2c1e4db5f9a50ecf061735a04a4d2b97d6dedeb
HardE <74683695+hard-e@users.noreply.github.com>
2021-03-31 12:10:21 -0400

    Add files via upload
    AdamWest - Inventory Management is a C# application created in Visual Studio for course WGU course c968. It demonstrates object oriented design and data grid view functionality.

---
+++ AdamWest - InventorySystem/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace Adam_West___InventorySystem
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(148, 134);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(75, 23);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "Click";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMain);
+            this.Name = "MainScreen";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMain;
+    }
+}
+
